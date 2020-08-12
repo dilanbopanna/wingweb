6b060280860e610af4790958bbd48dd06dcad337
dilanbopanna <bopannamj@gmail.com>
2020-08-12 17:39:43 +0530

    team

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,28 @@
 import React from 'react'
 import gururaj from '../assests/images/gururaj.jpeg'
+import aishwarya from '../assests/images/aishwarya.jpeg'
+import anjali from '../assests/images/anjali.jpeg'
+import anusha_k from '../assests/images/anusha_k.jpeg'
+import chetan from '../assests/images/chetan.jpeg'
+import chetana from '../assests/images/chetana.jpeg'
+import gagana_s from '../assests/images/gagana_s.jpeg'
+import gouthamb from '../assests/images/gouthamb.jpeg'
+import hamsaveni from '../assests/images/hamsaveni.jpeg'
+import harshitha_k from '../assests/images/harshitha_k.jpeg'
+import janhavi from '../assests/images/janhavi.jpeg'
+import megha_v from '../assests/images/megha_v.jpeg'
+import poornima from '../assests/images/poornima.jpeg'
+import poornima_t from '../assests/images/gururaj.jpeg'
+import praveen from '../assests/images/praveen.jpeg'
+import pushkin from '../assests/images/pushkin.jpeg'
+import shashank from '../assests/images/shashank.jpeg'
+import swathi_bh from '../assests/images/swathi_bh.jpeg'
+import trupti from '../assests/images/trupti.jpeg'
+import veena from '../assests/images/veena.jpeg'
+import shradda from '../assests/images/shradda.jpeg'
+import prajwal from '../assests/images/prajwal.jpeg'
+import manasvi from '../assests/images/manasvi.jpeg'
+import bops from '../assests/images/bops.jpg'
 
 import Cardinfo from '../components/cardinfo'
 import Scardinfo from '../components/scardinfo'
@@ -18,7 +41,7 @@ const Card = (props) => {
   <Cardinfo 
   image={gururaj}
   name='Dr Gururaj H L'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  about='Associate Professor, Department of Computer Science and Engineering'
   more='https://drive.google.com/file/d/1CnRGi3RS-aMABKlc7aVt3gfXWimtcHpO/view'
   linkedin='https://www.linkedin.com/in/dr-gururaj-h-l-92513539/'
   facebook=''
@@ -27,9 +50,9 @@ const Card = (props) => {
 
   <div class="column2">
   <Cardinfo 
-  image={gururaj}
-  name='Prof Janhavi H'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={janhavi}
+  name='Prof. Janhavi H'
+  about='Associate Professor, Department of Computer Science and Engineering'
   more='https://docs.google.com/a/vvce.ac.in/viewer?a=v&pid=sites&srcid=dnZjZS5hYy5pbnxjb21wdXRlci1zY2llbmNlLWFuZC1lbmdpbmVlcmluZ3xneDozZTg0M2MwNGNkZjI3NmIx'
   linkedin=''
   facebook=''
@@ -47,9 +70,9 @@ const Card = (props) => {
 <div class='row2'>
     <div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Hamsaveni M'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={hamsaveni}
+  name='Prof. Hamsaveni M'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://docs.google.com/a/vvce.ac.in/viewer?a=v&pid=sites&srcid=dnZjZS5hYy5pbnxjb21wdXRlci1zY2llbmNlLWFuZC1lbmdpbmVlcmluZ3xneDozYzg3NzUwNWM4ZDk3NTFl'
   linkedin=''
   facebook=''
@@ -57,56 +80,56 @@ const Card = (props) => {
     </div>
     <div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Rajini S'
-  about='Assistant professor, Department of Computer Science and Engineering'
-  more='https://drive.google.com/file/d/1xBxZWoN657ztMg-XUHzTuLxfkhSMce29/view'
-  linkedin=''
-  facebook=''
-  />
-</div>
-<div class='column2'>
-<Cardinfo 
-  image={gururaj}
-  name='Prof Poornima N'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={poornima}
+  name='Prof. Poornima N'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://drive.google.com/file/d/1WjdlHuRrICZQAfTgodtsF0VMnEfXfhc8/view'
   linkedin=''
   facebook=''
   />
 </div>
-    
-</div>
-<div class='row2'>
-    <div class='column2'>
+<div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Chethana H T'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={chetana}
+  name='Prof. Chethana H T'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://docs.google.com/a/vvce.ac.in/viewer?a=v&pid=sites&srcid=dnZjZS5hYy5pbnxjb21wdXRlci1zY2llbmNlLWFuZC1lbmdpbmVlcmluZ3xneDozYTcyNmVjMjg0ZjJmYmI1'
   linkedin=''
   facebook=''
   />
-    </div>
-    <div class='column2'>
+    
+</div>
+</div>
+<div class='row2'>
+<div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Praveena K S'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={praveen}
+  name='Prof. Praveen K S'
+  about='Assistant Professor, Department of Electronics and Communication Engineering'
   more='https://drive.google.com/file/d/16tElBJf-OYrM50z1F39fkSKPmDIl6d-M/view'
   linkedin=''
   facebook=''
   />
 </div>
 <div class='column2'>
+<Cardinfo 
+  image={shashank}
+  name='Prof. Shashank N'
+  about='Assistant Professor, Department of Computer Science and Engineering'
+  more='https://drive.google.com/file/d/0B5xUuteK0ddjSmdoTkZBVDlkNGdJS1lCTmNKTC12dE1DMVBz/view'
+  linkedin=''
+  facebook=''
+  />
+  </div>
+<div class='column2'>
 
 </div>
 <div class='column2'>
 <Cardinfo 
-  image={gururaj}
-  name='Prof Shashank N'
-  about='Assistant professor, Department of Computer Science and Engineering'
-  more='https://drive.google.com/file/d/0B5xUuteK0ddjSmdoTkZBVDlkNGdJS1lCTmNKTC12dE1DMVBz/view'
+  image={harshitha_k}
+  name='Prof. Harshitha K'
+  about='Assistant Professor, Department of Computer Science and Engineering'
+  more=''
   linkedin=''
   facebook=''
   />
@@ -115,9 +138,9 @@ const Card = (props) => {
 <div class='row2'>
     <div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Swathi B H'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={swathi_bh}
+  name='Prof. Swathi B H'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://drive.google.com/file/d/1j9LqeDsmR5laFrwkR9-xgXa9OiedhpH0/view'
   linkedin=''
   facebook=''
@@ -125,9 +148,9 @@ const Card = (props) => {
     </div>
     <div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Megha V'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={megha_v}
+  name='Prof. Megha V'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://drive.google.com/file/d/1aN62rGs0CKVRfpd5MmWxFYqOX6FMf78-/view'
   linkedin=''
   facebook=''
@@ -135,9 +158,9 @@ const Card = (props) => {
 </div>
 <div class='column2'>
 <Cardinfo 
-  image={gururaj}
-  name='Prof Anusha K S'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={anusha_k}
+  name='Prof. Anusha K S'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://drive.google.com/file/d/1C2bddI1wcJ_pjT13aPbbXr_6ct2xHTwW/view'
   linkedin=''
   facebook=''
@@ -148,9 +171,9 @@ const Card = (props) => {
 <div class='row2'>
     <div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof Aishwarya T'
-  about='Assistant professor, Department of Computer Science and Engineering'
+  image={aishwarya}
+  name='Prof. Aishwarya T'
+  about='Assistant Professor, Department of Computer Science and Engineering'
   more='https://drive.google.com/file/d/1xMczLnBHkL6C2zKA3rMV6I0nei4BLVsY/view'
   linkedin=''
   facebook=''
@@ -158,17 +181,32 @@ const Card = (props) => {
     </div>
     <div class='column2'>
     <Cardinfo 
-  image={gururaj}
-  name='Prof. Aishwarya T'
-  about='Assistant professor, Department of Computer Science and Engineering'
-  more='https://drive.google.com/file/d/1xMczLnBHkL6C2zKA3rMV6I0nei4BLVsY/view'
+  image={gagana_s}
+  name='Prof. Gagana S'
+  about='Assistant Professor, Department of Computer Science and Engineering'
+  more=''
   linkedin=''
   facebook=''
   />
 </div>
 <div class='column2'>
 <Cardinfo 
-  image={gururaj}
+  image={gouthamb}
+  name='Prof. Goutham B'
+  about='Assistant Professor, Department of  Electrical and Electronics Engineering'
+  more=''
+  linkedin=''
+  facebook=''
+  />
+</div>
+
+</div>
+<div class='row2'>
+   
+   
+<div class='column2'>
+<Cardinfo 
+  image={pushkin}
   name='Pushkin K M'
   about='Assistant Instructor, Department of Computer Science and Engineering'
   more=''
@@ -185,27 +223,27 @@ const Card = (props) => {
 <div class='row2'>
     <div class='column2'>
     <Scardinfo 
-  image={gururaj}
-  name='dcasc'
-  about='scascasc, Department of Computer Science and Engineering'
+  image={manasvi}
+  name='Manasvi J Maasthi'
+  about='General Secretary, Department of Computer Science and Engineering'
   linkedin=''
   facebook=''
   />
     </div>
     <div class='column2'>
     <Scardinfo 
-  image={gururaj}
-  name='ascasc'
-  about='ascasc, Department of Computer Science and Engineering'
+  image={prajwal}
+  name='Prajwal Hebbar A S'
+  about='External Affairs, Department of Computer Science and Engineering'
   linkedin=''
   facebook=''
   />
 </div>
 <div class='column2'>
 <Scardinfo 
-  image={gururaj}
-  name='ascasc'
-  about='ascasc, Department of Computer Science and Engineering'
+  image={trupti}
+  name='Trupthi Raghu'
+  about='Sponsorship Co-Ordinator, Department of Computer Science and Engineering'
   linkedin=''
   facebook=''
   />
@@ -213,6 +251,57 @@ const Card = (props) => {
     
 </div>
 
+<div class='row2'>
+    <div class='column2'>
+    <Scardinfo 
+  image={shradda}
+  name='Shraddha'
+  about='Membership Co-Ordinator, Department of Computer Science and Engineering'
+  linkedin=''
+  facebook=''
+  />
+    </div>
+    <div class='column2'>
+    <Scardinfo 
+  image={veena}
+  name='Veena H'
+  about='Event Co-Ordinator, Department of Computer Science and Engineering'
+  linkedin=''
+  facebook=''
+  />
+</div>
+<div class='column2'>
+<Scardinfo 
+  image={chetan}
+  name='Chetan S S'
+  about='Technical Co-Ordinator, Department of Computer Science and Engineering'
+  linkedin=''
+  facebook=''
+  />
+</div>
+    
+</div>
+<div class='row2'>
+    <div class='column2'>
+    <Scardinfo 
+  image={anjali}
+  name='Anjali M'
+  about='Internal Affairs, Department of Computer Science and Engineering'
+  linkedin=''
+  facebook=''
+  />
+    </div>
+    <div class='column2'>
+    <Scardinfo 
+  image={bops}
+  name='Bopanna M J'
+  about='Web Designer, Department of Computer Science and Engineering'
+  linkedin=''
+  facebook=''
+  />
+</div>
+
+</div>
         </div>
         
     );
